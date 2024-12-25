8f62826ba34aa3077a40ede05fb0b93950c67260
assmabaziz <assma.baziz@yahoo.com>
2024-12-26 00:39:21 +0300

    module user and manager deleted

--- src/app/features/dashboard/dashboard-routing.module.ts
+++ src/app/features/dashboard/dashboard-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 
-const routes: Routes = [{ path: '', component: DashboardComponent }, { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) }, { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) }];
-
+const routes: Routes = [
+  { path: '', component: DashboardComponent }
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
