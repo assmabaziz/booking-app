2eb492de6682b4f5894d35446e2bd8cec2ea79b3
Hossam <hossamabdo7359@gmail.com>
2024-12-26 20:55:31 +0200

    [feat] dashboard Module and sideBar component: create structure of dashboard and create sidebar (design and routing)

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,36 +1,44 @@
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { jwtDecode } from 'jwt-decode';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-role: string|null = ''
-  constructor(private _HttpClient:HttpClient) {
+  role: string | null = '';
+  constructor(private _HttpClient: HttpClient, private _Router: Router) {
     // if(localStorage.getItem('userToken') ){
     //   this.getProfile();
     // }
-   }
-   getProfile(){
+  }
+  getProfile() {
     let finalToken: any = localStorage.getItem('userToken');
-    let decodedToken: any = jwtDecode(finalToken)
+    let decodedToken: any = jwtDecode(finalToken);
     localStorage.setItem('userName', decodedToken.userName);
     localStorage.setItem('userGroup', decodedToken.userGroup);
-    this.setRole()
+    this.setRole();
   }
-  setRole(){
-    if (localStorage.getItem('userToken') !== null && localStorage.getItem('role')) {
-      this.role = localStorage.getItem('role')
+  setRole() {
+    if (
+      localStorage.getItem('userToken') !== null &&
+      localStorage.getItem('role')
+    ) {
+      this.role = localStorage.getItem('role');
     }
   }
-  onSignUp(data:any):Observable<any>{
-    return this._HttpClient.post('portal/users',data)
+  onSignUp(data: any): Observable<any> {
+    return this._HttpClient.post('portal/users', data);
   }
 
-onSignin(data:any):Observable<any>
- {
-  return this._HttpClient.post<any>('/api/v0/admin/users/login', data)
- }
+  onSignin(data: any): Observable<any> {
+    return this._HttpClient.post<any>('/api/v0/admin/users/login', data);
+  }
+
+  onLogout() {
+    localStorage.clear();
+    this._Router.navigate(['/auth']);
+  }
 }
