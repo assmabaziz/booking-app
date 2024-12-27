a546d60131f942a48955af6e186d5a29a8b2bea3
Hossam <hossamabdo7359@gmail.com>
2024-12-27 18:12:52 +0200

    [feat] home component :create chart design

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgxEchartsModule } from 'ngx-echarts';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { HomeComponent } from './components/home/home.component';
@@ -9,6 +9,13 @@ import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [DashboardComponent, HomeComponent],
-  imports: [CommonModule, DashboardRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    SharedModule,
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts'),
+    }),
+  ],
 })
 export class DashboardModule {}
