adbc1ef3af1f3d87f9e13f460d32e9f5ca329e3b
assmabaziz <assma.baziz@yahoo.com>
2024-12-29 09:33:13 +0300

    [feat]:module rooms added with interface, srvice and components

---
+++ src/app/features/dashboard/modules/rooms/components/add-edit-room/add-edit-room.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-room',
+  templateUrl: './add-edit-room.component.html',
+  styleUrl: './add-edit-room.component.scss'
+})
+export class AddEditRoomComponent {
+
+}
