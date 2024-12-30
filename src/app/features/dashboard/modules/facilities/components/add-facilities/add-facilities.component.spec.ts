03aeeba6950f72a9c220783dca4495396406ee5f
romany <romanyghttas332@gmail.com>
2024-12-30 17:56:08 +0200

    [feat] facilitires module : create add facility component

---
+++ src/app/features/dashboard/modules/facilities/components/add-facilities/add-facilities.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddFacilitiesComponent } from './add-facilities.component';
+
+describe('AddFacilitiesComponent', () => {
+  let component: AddFacilitiesComponent;
+  let fixture: ComponentFixture<AddFacilitiesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AddFacilitiesComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(AddFacilitiesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
