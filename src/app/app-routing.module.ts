9bf051db62e2a619dfdf4b93ed7507c0e85904cd
Hossam <hossamabdo7359@gmail.com>
2025-01-01 04:12:24 +0200

    [feat] user gaurd: edit in user gaurd and sone edits in charts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth guard/auth.guard';
 import { adminGuard } from './core/guards/admin guard/admin.guard';
+import { userGuard } from './core/guards/user guard/user.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
@@ -21,6 +22,7 @@ const routes: Routes = [
   },
   {
     path: 'landing-page',
+    canActivate: [userGuard],
     loadChildren: () =>
       import('./features/landing-page/landing-page.module').then(
         (m) => m.LandingPageModule
