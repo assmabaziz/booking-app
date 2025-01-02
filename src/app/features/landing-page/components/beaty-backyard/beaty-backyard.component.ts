97137681b62492b5ad50e326b0dcc50303b828d7
assmabaziz <assma.baziz@yahoo.com>
2025-01-02 12:06:33 +0300

    components added

---
+++ src/app/features/landing-page/components/beaty-backyard/beaty-backyard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beaty-backyard',
+  templateUrl: './beaty-backyard.component.html',
+  styleUrl: './beaty-backyard.component.scss'
+})
+export class BeatyBackyardComponent {
+
+}
---
+++ src/app/features/landing-page/components/most-popular-ads/most-popular-ads.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-most-popular-ads',
+  templateUrl: './most-popular-ads.component.html',
+  styleUrl: './most-popular-ads.component.scss'
+})
+export class MostPopularAdsComponent {
+
+}
